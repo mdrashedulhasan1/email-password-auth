b8e70bd033225bae27378a3f52e1e1c17fa14fd0
unknown <rashedul15-1718@diu.edu.bd>
2022-08-18 00:12:55 -0700

    email-password-auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import app from './firebase.init';
 function App() {
+  const [registered, setRegistered] = useState(false);
+  const [user, setUser] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const auth = getAuth(app);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleEmailBlur = (event) => {
+    const email = event.target.value;
+    setEmail(email);
+    console.log(email);
+  }
+  const handlePasswordBlur = (event) => {
+    const password = event.target.value;
+    setPassword(password);
+    console.log(password);
+  }
+  const handleFormSubmit = (event) => {
+    if (registered) {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          setUser(user)
+          console.log(user)
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          console.log(errorCode);
+          const errorMessage = error.message;
+          setErrorMessage(errorMessage)
+        });
+    }
+    else {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          console.log(user);
+          setUser(user)
+          setEmail('')
+          setPassword('')
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          console.log(errorCode);
+          const errorMessage = error.message;
+          setErrorMessage(errorMessage)
+        });
+    }
+    event.preventDefault();
+  };
+  const handleRegisteredChanged = event => {
+    setRegistered(event.target.checked);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='w-50 mx-auto mt-5'>
+        <h1 className='text-primary'>{registered ? 'Login' : 'Please Register'}</h1>
+        <Form onSubmit={handleFormSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check onChange={handleRegisteredChanged} type="checkbox" label="Already Registered?" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            {registered ? 'Login' : 'Register'}
+          </Button>
+        </Form>
+        {errorMessage}
+      </div>
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAvT0m9bqnAUa9cUtULGrPl6J2B-nqEu_E",
+  authDomain: "email-password-auth-c7836.firebaseapp.com",
+  projectId: "email-password-auth-c7836",
+  storageBucket: "email-password-auth-c7836.appspot.com",
+  messagingSenderId: "1067689560088",
+  appId: "1:1067689560088:web:f8da363bd39afb43bb1b6d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
